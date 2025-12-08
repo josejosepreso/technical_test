1ea4ea5c4360ca2b555b997ff4af9b6022158669
josejosepreso <josehihat@gmail.com>
2025-12-07 18:22:00 -0600

    add get products by category endpoint

--- shoppingcart-products/src/main/java/hn/shoppingcart/shoppingcart_products/controller/ProductController.java
+++ shoppingcart-products/src/main/java/hn/shoppingcart/shoppingcart_products/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hn.shoppingcart.shoppingcart_products.ErrorResponse;
@@ -28,6 +29,14 @@ public class ProductController {
 	@GetMapping("/all")
 	public List<Product> getAll() {
 		return this.productService.getAll();
+	}
+
+	@GetMapping
+	public List<Product> getByCategory(@RequestParam String category) {
+		return this.productService.getAll()
+			.stream()
+			.filter(product -> product.getCategory().equals(category))
+			.toList();
 	}
 
 	@GetMapping("/{id}")
